dd5bb258fe84e6c53712015b2c0997f614329d3a
safarov-en <safarov.elkhan110@gmail.com>
2024-09-12 17:26:00 +0500

    Task styling

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,5 +1,11 @@
 import './Column.css'
+import Task from './Task'
 
 export default function Column({state}) {
-    return <div className="column">{state}</div>
+    return (
+        <div className="column">
+            <p>{state}</p>
+            <Task title="Todo" />
+        </div>
+    )
 }

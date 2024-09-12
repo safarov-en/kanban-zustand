f5b1c941af187b7b635a328255231cefa9efa002
safarov-en <safarov.elkhan110@gmail.com>
2024-09-12 18:11:05 +0500

    Adding a store

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,7 +1,11 @@
+import { useStore } from 'zustand'
 import './Column.css'
 import Task from './Task'
 
 export default function Column({state}) {
+    const tasks = useStore((store) =>
+        store.tasks.filter((task) => task.state === state)
+    )
     return (
         <div className="column">
             <p>{state}</p>

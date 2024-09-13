3ce9d9d77a7a53c89288188d5ccadbd40c72a249
safarov-en <safarov.elkhan110@gmail.com>
2024-09-13 12:32:16 +0500

    Adding Tasks form

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
 import { useStore } from '../store'
 import './Column.css'
 import Task from './Task'
 import { shallow } from 'zustand/shallow'
 
 export default function Column({state}) {
+    const [text, setText] = useState('')
+    const [open, setOpen] = useState(false)
     const tasks = useStore((store) =>
         store.tasks.filter((task) => task.state === state),
         shallow
@@ -13,11 +16,25 @@ export default function Column({state}) {
         <div className="column">
             <div className='titleWrapper'>
                 <p>{state}</p>
-                <button onClick={() => addTask('gsrthrth' + state, state)}>Add</button>
+                <button onClick={() => setOpen(true)}>Add</button>
             </div>
             {tasks.map((task) => (
                 <Task title={task.title} key={task.title} />
             ))}
+            {open && <div className='Modal'>
+                <div className='modalContent'>
+                    <input onChange={(e) => setText(e.target.value)} value={text} />
+                    <button
+                        onClick={() => {
+                            addTask(text, state)
+                            setText('')
+                            setOpen(false)
+                        }}
+                    >
+                        Submit
+                    </button>
+                </div>
+            </div>}
         </div>
     )
 }

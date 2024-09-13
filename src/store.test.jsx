a198c682d889cf01bd30e823d6331ebd62ba092d
safarov-en <safarov.elkhan110@gmail.com>
2024-09-13 16:51:18 +0500

    Checking adding an element

--- src/store.test.jsx
+++ src/store.test.jsx
@@ -15,3 +15,17 @@ test('should return default value at the start', () => {
     render(<TestComponent selector={selector} effect={effect} />)
     expect(effect).toHaveBeenCalledWith([])
 })
+
+test('should add an items to the store and rerun the effect', () => {
+    const selector = (store) => ({tasks: store.tasks, addTask: store.addTask})
+    const effect = vi.fn().mockImplementation((items) => {
+        if(items.tasks.length === 0) {
+            items.addTask('a', 'b')
+        }
+    })
+    render(<TestComponent selector={selector} effect={effect} />)
+    expect(effect).toHaveBeenCalledTimes(2)
+    expect(effect).toHaveBeenCalledWith(
+        expect.objectContaining({tasks: [{title: 'a', state: 'b'}]})
+    )
+})

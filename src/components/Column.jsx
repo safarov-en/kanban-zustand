f0453b4f6d6007cc7e8c8f68fef29bc68f755e0e
safarov-en <safarov.elkhan110@gmail.com>
2024-09-13 13:53:42 +0500

    Dragging and dropping tasks

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -12,8 +12,20 @@ export default function Column({state}) {
         shallow
     )
     const addTask = useStore((store) => store.addTask)
+    const setDraggedTask = useStore((store) => store.setDraggedTask)
+    const draggedTask = useStore((store) => store.draggedTask)
+    const moveTask = useStore((store) => store.moveTask)
     return (
-        <div className="column">
+        <div
+            className="column"
+            onDragOver={(e) => {
+                e.preventDefault()
+            }}
+            onDrop={(e) => {
+                moveTask(draggedTask, state)
+                setDraggedTask(null)
+            }}
+        >
             <div className='titleWrapper'>
                 <p>{state}</p>
                 <button onClick={() => setOpen(true)}>Add</button>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -9,9 +9,16 @@ export default function Task({title}) {
         store.tasks.find((task) => task.title === title),
         shallow
     )
+    const setDraggedTask = useStore((store) => store.setDraggedTask)
     const deleteTask = useStore((store) => store.deleteTask)
     return (
-        <div className="task">
+        <div
+            className="task"
+            draggable
+            onDragStart={() => {
+                setDraggedTask(task.title)
+            }}
+        >
             <div>{task.title}</div>
             <div className='bottomWrapper'>
                 <div>

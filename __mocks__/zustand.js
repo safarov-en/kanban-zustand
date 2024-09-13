b420a840dc62034d036a99c218f3cee4ce96c79a
safarov-en <safarov.elkhan110@gmail.com>
2024-09-13 17:24:21 +0500

    Checking deleting an item

---
+++ __mocks__/zustand.js
@@ -0,0 +1,16 @@
+import {act} from 'react-dom/test-utils'
+import { beforeEach, vi } from 'vitest'
+const {create: actualCreate} = await vi.importActual('zustand')
+
+const storeResetFns = new Set()
+
+export const create = (createState) => {
+    const store = actualCreate(createState)
+    const initialState = store.getState()
+    storeResetFns.add(() => store.setState(initialState, true))
+    return store
+}
+
+beforeEach(() => {
+    act(() => storeResetFns.forEach((resetFn) => resetFn()))
+})
--- src/store.test.jsx
+++ src/store.test.jsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react"
 import { useStore } from "./store"
-import { expect } from "vitest"
+import { vi } from "vitest"
 import { render } from "@testing-library/react"
+
+vi.mock('zustand')
 
 function TestComponent({selector, effect}) {
     const items = useStore(selector)
@@ -28,4 +30,26 @@ test('should add an items to the store and rerun the effect', () => {
     expect(effect).toHaveBeenCalledWith(
         expect.objectContaining({tasks: [{title: 'a', state: 'b'}]})
     )
+})
+
+test('should add an items to the store and rerun the effect', () => {
+    const selector = (store) => ({
+        tasks: store.tasks,
+        addTask: store.addTask,
+        deleteTask: store.deleteTask
+    })
+    let createdTask = false
+    let currentItems
+    const effect = vi.fn().mockImplementation((items) => {
+        currentItems = items
+        if(!createdTask) {
+            items.addTask('a', 'b')
+            createdTask = true
+        } else if(items.tasks.length === 1) {
+            items.deleteTask('a')
+        }
+    })
+    render(<TestComponent selector={selector} effect={effect} />)
+    expect(effect).toHaveBeenCalledTimes(3)
+    expect(currentItems.tasks).toEqual([])
 })
